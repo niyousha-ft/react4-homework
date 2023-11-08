1b913107cf7972a356b3d045bbb9e1a1d719043d
niyousha-ft <niyoushaft@gmail.com>
2023-11-08 14:02:38 +0330

    create WeatherForecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecastData, setForecastData] = useState(null);
+
+  function getForecastData(response) {
+    setForecastData(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiKey = "b35c686ba9565ba0ab254c2230937552";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(getForecastData);
+  }
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecastData.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+
+    return null;
+  }
+}
