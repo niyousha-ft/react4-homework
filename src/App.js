ddfa6af41e4f2c2887094fcebb27b8ba929143c3
niyousha-ft <niyoushaft@gmail.com>
2023-11-09 01:29:05 +0330

    Add background color css and js in index.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ function App() {
     <div className="App">
       <div className="container">
         <Search defaultCity="Tehran" />
-        <footer>
-          This is open-source in{" "}
-          <a
-            href="https://github.com/niyousha-ft/react4-homework"
-            target="_blank"
-          >
-            <strong>Github</strong>
-          </a>{" "}
-          by <strong> Niyousha Eftekhari</strong>
-        </footer>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,11 +5,29 @@ import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 
+const currentTime = new Date().getHours();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="container">
-      <App />
+    <div
+      className={`weather-app ${
+        currentTime >= 20 ? "dark-bg" : currentTime <= 6 ? "light-bg" : "med-bg"
+      }`}
+    >
+      <div className="container">
+        <App />
+        <footer>
+          This is open-source in{" "}
+          <a
+            href="https://github.com/niyousha-ft/react4-homework"
+            target="_blank"
+          >
+            <strong>Github</strong>
+          </a>{" "}
+          by <strong> Niyousha Eftekhari</strong>
+        </footer>
+      </div>
     </div>
   </React.StrictMode>
 );
